65f9d4a620db1646ab776126e0f20b2d2b3bbafe
kincsojerne <voros.kincsojerne@gmail.com>
2019-08-05 14:58:12 +0200

    first test done

--- src/test/java/com/codecool/JiraTest.java
+++ src/test/java/com/codecool/JiraTest.java
@@ -3,7 +3,10 @@ package com.codecool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -29,5 +32,20 @@ public class JiraTest {
         JiraLogin.login("user13", "CoolCanvas19.");
         JiraLogin.logout();
     }
+
+
+    @Test
+    public void browseExistingIssuesWithSearch() throws InterruptedException {
+        JiraLogin.login("user13", "CoolCanvas19.");
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+        Thread.sleep(1000);
+        driver.findElement(By.id("find_link")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("issues_new_search_link_lnk")).click();
+        Thread.sleep(1000);
+        String actual = driver.findElement(By.id("jira-share-trigger")).getText();
+        assertEquals("Share this search by emailing other users Share", actual);
+    }
+
 
 }
