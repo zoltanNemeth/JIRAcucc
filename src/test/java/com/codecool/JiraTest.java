3228aa7315209ecce009380fccb7849c86ec8f4a
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-08-05 10:51:52 +0200

    Chromedriver added, Class JiraTest initialized

---
+++ src/test/java/com/codecool/JiraTest.java
@@ -0,0 +1,30 @@
+package com.codecool;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JiraTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver","chromedriver");
+        driver = new ChromeDriver();
+        driver.navigate().to("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void firstTest() {
+
+    }
+
+}
