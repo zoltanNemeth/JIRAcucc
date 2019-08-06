a6e8ecb73142d657b8af070f29bce5cb8b26abed
kincsojerne <voros.kincsojerne@gmail.com>
2019-08-06 11:29:25 +0200

    unsuccesful captcha login happy path

--- src/test/java/com/codecool/JiraTest.java
+++ src/test/java/com/codecool/JiraTest.java
@@ -3,6 +3,8 @@ package com.codecool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -28,6 +30,20 @@ public class JiraTest {
     public void LoginLogoutTest() throws InterruptedException {
         JiraLogin.login("user13", "CoolCanvas19.");
         JiraLogin.logout();
+    }
+
+    @Test
+    public void unsuccessfulLoginWithCaptcha() throws InterruptedException {
+        JiraLogin.login("user13", "asdfghjk");
+        Thread.sleep(100);
+        JiraLogin.login("user13", "asdfghjk");
+        Thread.sleep(100);
+        JiraLogin.login("user13", "asdfghjk");
+        Thread.sleep(100);
+        JiraLogin.login("user13", "CoolCanvas19.");
+        Thread.sleep(100);
+        String actual = driver.findElement(By.xpath("//form[@id='login-form']/div/div/p")).getText();
+        assertEquals("Sorry, your userid is required to answer a CAPTCHA question correctly.", actual);
     }
 
 }
