be45f93b011e05eb665c2eda438513b8010c2a2f
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-08-06 10:22:24 +0200

    a draft test for UnsuccessfulLoginWithInvalidValues - without assertion

--- src/test/java/com/codecool/JiraTest.java
+++ src/test/java/com/codecool/JiraTest.java
@@ -3,7 +3,9 @@ package com.codecool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,6 +30,28 @@ public class JiraTest {
     public void LoginLogoutTest() throws InterruptedException {
         JiraLogin.login("user13", "CoolCanvas19.");
         JiraLogin.logout();
+    }
+
+    @Test
+    public void UnsuccessfulLoginWithInvalidValues() throws InterruptedException {
+        String errorMsg = "Sorry, your username and password are incorrect - please try again.";
+
+        JiraLogin.login("user2019", "CoolCanvas19.");
+        Thread.sleep(500);
+        //assertEquals(errorMsg, driver.findElement(By.className("aui-message error")));
+        //assertEquals(errorMsg, driver.findElement(By.xpath("//*[@class='aui-message error']//*[text()='Sorry, your username and password are incorrect - please try again.']")));
+        //assertEquals(errorMsg, driver.findElement(By.xpath("//p[text(),'Sorry, your username and password are incorrect - please try again.']")));
+        //assertEquals(errorMsg, driver.findElement(By.xpath("//p[contains(text(),'Sorry, your username and password are incorrect - please try again.')]"))); // ***
+
+        JiraLogin.login("user14", "");
+        Thread.sleep(500);
+
+        JiraLogin.login("", "CoolCanvas19.");
+        Thread.sleep(500);
+
+        JiraLogin.login("", "");
+        Thread.sleep(500);
+
     }
 
 }
