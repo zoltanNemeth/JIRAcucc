d580de832fe965ba30e5263655d50c513e7db62f
Lizi Boros <boroslizi@gmail.com>
2019-08-06 20:29:55 +0200

    TOUCAN done (fails, because there isn't a "TOUCAN-2" issue)

--- src/test/java/com/codecool/JiraTest.java
+++ src/test/java/com/codecool/JiraTest.java
@@ -3,7 +3,11 @@ package com.codecool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,9 +29,20 @@ public class JiraTest {
     }
 
     @Test
-    public void LoginLogoutTest() throws InterruptedException {
-        JiraLogin.login("user13", "CoolCanvas19.");
-        JiraLogin.logout();
+    public void toucanProjectContainsIssues() throws InterruptedException {
+        String password = "CoolCanvas19.";
+        JiraLogin.login("user14", password);
+        driver.get("https://jira.codecool.codecanvas.hu/browse/TOUCAN-65?jql=project%20%3D%20TOUCAN");
+
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+
+        By issueOneXPath = By.xpath("//div[@class='list-content']/ol/li[@data-key='TOUCAN-1']");
+        By issueTwoXPath = By.xpath("//div[@class='list-content']/ol/li[@data-key='TOUCAN-2']");
+        By issueThreeXPath = By.xpath("//div[@class='list-content']/ol/li[@data-key='TOUCAN-3']");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(issueOneXPath));
+        wait.until(ExpectedConditions.presenceOfElementLocated(issueTwoXPath));
+        wait.until(ExpectedConditions.presenceOfElementLocated(issueThreeXPath));
     }
 
 }
