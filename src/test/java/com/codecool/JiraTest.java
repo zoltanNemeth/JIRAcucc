1b4a7081d885c7f06c69ddc517337509088bd4f4
Lizi Boros <boroslizi@gmail.com>
2019-08-06 14:58:59 +0200

    Manage components test automated

--- src/test/java/com/codecool/JiraTest.java
+++ src/test/java/com/codecool/JiraTest.java
@@ -3,8 +3,14 @@ package com.codecool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class JiraTest {
@@ -25,9 +31,67 @@ public class JiraTest {
     }
 
     @Test
-    public void LoginLogoutTest() throws InterruptedException {
+    public void manageComponents() throws InterruptedException {
+        String recordName = "JIRAcucc";
+        String recordDescription = "JIRAdescription";
+        WebElement newRecord = null;
+
         JiraLogin.login("user13", "CoolCanvas19.");
-        JiraLogin.logout();
+        driver.get("https://jira.codecool.codecanvas.hu/plugins/servlet/project-config/PP1/components");
+
+        String expectedProjectName = "Private Project 1";
+        String actualProjectName = driver.findElement(By.linkText(expectedProjectName)).getText();
+        assertEquals(expectedProjectName, actualProjectName);
+
+        driver.findElement(By.name("name")).sendKeys(recordName);
+        driver.findElement(By.name("description")).sendKeys(recordDescription);
+        WebElement table = driver.findElement(By.id("project-config-components-table"));
+        WebElement addButton = table.findElement(By.xpath("//tbody/tr[1]/td[6]/input[@value='Add']"));
+        addButton.click();
+
+        By xPath = By.xpath(String.format("//table[@id='project-config-components-table']/tbody/tr[@data-name='%s']", recordName));
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.presenceOfElementLocated(xPath));
+        List<WebElement> tableRows = table.findElements(By.xpath("//tbody/tr"));
+
+        for (WebElement row : tableRows) {
+            if (row.getText().contains(recordName)) {
+                newRecord = row;
+            }
+        }
+
+        assert newRecord != null;
+
+        String recordId = newRecord.getAttribute("data-id");
+        assertNotNull(table.findElement(By.xpath(String.format("//tbody/tr[@data-id='%s']", recordId))));
+
+        driver.get("https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.codecanvas.glass:glass");
+        assertNotNull(driver.findElement(By.linkText(recordName)));
+
+        driver.get("https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.atlassian.jira.jira-projects-plugin:components-page");
+        actualProjectName = driver.findElement(By.linkText(expectedProjectName)).getText();
+        assertEquals(expectedProjectName, actualProjectName);
+
+        table = driver.findElement(By.id("components-table"));
+        newRecord = null;
+        tableRows = table.findElements(By.xpath("//tbody/tr"));
+        for (WebElement row : tableRows) {
+            if (row.getText().contains(recordName)) {
+                newRecord = row;
+            }
+        }
+        assert newRecord != null;
+
+        table.findElement(By.xpath(String.format("//tbody/tr[@data-component-id='%s']/td[6]/div/a", recordId))).click();
+        driver.findElement(By.linkText("Delete")).click();
+        driver.findElement(By.xpath("//input[@id='submit']")).click();
+
+        driver.get("https://jira.codecool.codecanvas.hu/projects/PP1?selectedItem=com.codecanvas.glass:glass");
+
+        tableRows = driver.findElements(By.xpath("//table[@id='components-table']/tbody/tr"));
+        for (WebElement row : tableRows) {
+            assertFalse(row.getText().contains(recordName));
+        }
     }
 
 }
