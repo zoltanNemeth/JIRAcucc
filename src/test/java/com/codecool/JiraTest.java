a7b4e73e0e30019af8754f38d91a70675be89f60
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-08-07 14:48:05 +0200

    Refactoring

--- src/test/java/com/codecool/JiraTest.java
+++ src/test/java/com/codecool/JiraTest.java
@@ -3,6 +3,7 @@ package com.codecool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
