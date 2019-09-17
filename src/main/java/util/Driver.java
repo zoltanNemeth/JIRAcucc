2a4cbc21dcf1b03d54e115f7ef285267d5f13df0
tejfi <adamfdeak@gmail.com>
2019-09-17 12:53:39 +0200

    try with firefox

--- src/main/java/util/Driver.java
+++ src/main/java/util/Driver.java
@@ -2,6 +2,7 @@ package util;
 
 import com.github.shyiko.dotenv.DotEnv;
 import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,34 +12,39 @@ import java.net.URL;
 import java.util.Map;
 
 public class Driver {
-        private WebDriver webDriver;
-        private WebDriverWait driverWait;
-        private String baseRoute = "https://jira.codecool.codecanvas.hu/";
-        private URL remoteUrl;
-        private DesiredCapabilities capabilities;
+    private WebDriver webDriver;
+    private WebDriverWait driverWait;
+    private String baseRoute = "https://jira.codecool.codecanvas.hu/";
+    private URL remoteUrl;
+    private DesiredCapabilities capabilities;
 
-        {
-                try {
-                        remoteUrl = new URL("http://" + System.getProperty("JENKINS_USERNAME") + ":" + System.getProperty("JENKINS_PW") + "@" + System.getProperty("JENKINS_BASE_URL"));
-                } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                }
+    {
+        try {
+            remoteUrl = new URL("http://" + System.getProperty("JENKINS_USERNAME") + ":" + System.getProperty("JENKINS_PW") + "@" + System.getProperty("JENKINS_BASE_URL"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
+    }
 
-        public WebDriver getWebDriver() {
-                this.capabilities = DesiredCapabilities.chrome();
-                this.webDriver = new RemoteWebDriver(remoteUrl, capabilities);
-                this.driverWait = new WebDriverWait(webDriver, 10);
-                this.webDriver.manage().window().maximize();
-                return this.webDriver;
-        }
-        public void goTo(String route) {
-                webDriver.navigate().to(baseRoute + route);
-                this.driverWait = new WebDriverWait(webDriver, 10);
-        }
-        public String getRoute(String route) {
-                return baseRoute + route;
-        }
+    public WebDriver getWebDriver() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        capabilities.setPlatform(Platform.LINUX);
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--start-maximized");
+        firefoxOptions.merge(capabilities);
+        webDriver = new RemoteWebDriver(remoteUrl, firefoxOptions);
+        return webDriver;
+    }
+
+    public void goTo(String route) {
+        webDriver.navigate().to(baseRoute + route);
+        this.driverWait = new WebDriverWait(webDriver, 10);
+    }
+
+    public String getRoute(String route) {
+        return baseRoute + route;
+    }
 
 
 }
