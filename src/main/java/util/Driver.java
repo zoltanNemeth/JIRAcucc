a178544af31e7408fb7bc97294c25a67813a3503
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-09-19 12:51:55 +0200

    System.getProperty changed to System.getenv

--- src/main/java/util/Driver.java
+++ src/main/java/util/Driver.java
@@ -17,7 +17,7 @@ public class Driver {
 
     {
         try {
-            remoteUrl = new URL("https://" + System.getProperty("JENKINS_USERNAME") + ":" + System.getProperty("JENKINS_PW") + "@" + System.getProperty("JENKINS_BASE_URL"));
+            remoteUrl = new URL("https://" + System.getenv("JENKINS_USERNAME") + ":" + System.getenv("JENKINS_PW") + "@" + System.getenv("JENKINS_BASE_URL"));
             System.out.println(remoteUrl);
         } catch (MalformedURLException e) {
             e.printStackTrace();
